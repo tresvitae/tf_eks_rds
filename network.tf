4691daa5ea071bb254a406e37e8f7ff423a63d75
Patryk Futa <patryk.futa@gmail.com>
2021-12-14 17:55:05 +0100

    Set subnets

--- network.tf
+++ network.tf
@@ -8,3 +8,45 @@ resource "aws_vpc" "my_vpc" {
     Environment = var.environment
   }
 }
+
+resource "aws_subnet" "private" {
+  for_each = {
+    for subnet in local.private_nested_config : "${subnet.name}" => subnet
+  }
+
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = false
+
+  tags = {
+    Environment = var.environment
+    Name        = "${each.value.name}-${var.environment}"
+    "kubernetes.io/role/internal-elb" = each.value.eks ? "1" : ""
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "aws_subnet" "public" {
+  for_each = {
+    for subnet in local.public_nested_config : "${subnet.name}" => subnet
+  }
+
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Environment = var.environment
+    Name        = "${each.value.name}-${var.environment}"
+    "kubernetes.io/role.elb" = each.value.eks ? "1" : ""
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "region" {
     type = string
-    default = "eu-west-1"
 }
 
 variable "vpc_cidr_block" {
@@ -10,4 +9,103 @@ variable "vpc_cidr_block" {
 variable "environment" {
   type = string
   default = "dev"
+}
+
+variable "private_network_config" {
+  type = map(object({
+    cidr_block               = string
+    az                       = string
+    associated_public_subnet = string
+    eks                      = bool
+  }))
+  default = {
+    "private-eks-1" = {
+      cidr_block               = "10.0.0.0/23"
+      az                       = "eu-west-1a"
+      associated_public_subnet = "public-eks-1"
+      eks                      = true
+    },
+    "private-eks-2" = {
+      cidr_block               = "10.0.2.0/23"
+      az                       = "eu-west-1b"
+      associated_public_subnet = "public-eks-2"
+      eks                      = true
+    },
+    "private-rds-1" = {
+      cidr_block               = "10.0.10.0/24"
+      az                       = "eu-west-1a"
+      associated_public_subnet = ""
+      eks                      = false
+    },
+    "private-rds-2" = {
+      cidr_block               = "10.0.11.0/24"
+      az                       = "eu-west-1b"
+      associated_public_subnet = ""
+      eks                      = false
+    }
+  }
+}
+
+locals {
+  private_nested_config = flatten([
+    for name, config in var.private_network_config : [
+      {
+        name                     = name
+        cidr_block               = config.cidr_block
+        az                       = config.az
+        associated_public_subnet = config.associated_public_subnet
+        eks                      = config.eks
+      }
+    ]
+  ])
+}
+
+variable "public_network_config" {
+    type = map(object({
+        cidr_block = string
+        az         = string
+        nat_gw     = bool
+        eks        = bool
+    }))
+
+    default = {
+      "public-eks-1" = {
+        cidr_block = "10.0.4.0/23"
+        az         = "eu-west-1a"
+        nat_gw     = true
+        eks        = true
+      },
+      "public-eks-2" = {
+        cidr_block = "10.0.6.0/23"
+        az         = "eu-west-1b"
+        nat_gw     = true
+        eks        = true
+      },
+      "public-rds-1" = {
+        cidr_block = "10.0.12.0/24"
+        az         = "eu-west-1a"
+        nat_gw     = false
+        eks        = false
+      },
+      "public-rds-2" = {
+        cidr_block = "10.0.13.0/24"
+        az         = "eu-west-1b"
+        nat_gw     = false
+        eks        = false
+      }
+    }
+}
+
+locals {
+    public_nested_config = flatten([
+        for name, config in var.public_network_config : [
+            {
+                name                    = name
+                cidr_block              = config.cidr_block
+                az                      = config.az
+                nat_gw                  = config.nat_gw
+                eks                     = config.eks
+            }
+        ]
+    ])
 }
