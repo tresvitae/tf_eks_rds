1f82e36e412ba43d7d6bb172efd845905c317938
Patryk Futa <patryk.futa@gmail.com>
2021-12-15 15:19:34 +0100

    Add 2 node groups

--- eks.tf
+++ eks.tf
@@ -79,3 +79,112 @@ resource "aws_security_group_rule" "cluster_inblound" {
   to_port                  = 0
   type                     = "ingress"
 }
+
+resource "aws_eks_node_group" "private" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "private-node-group-${var.environment}"
+  node_role_arn   = aws_iam_role.node-group.arn
+  subnet_ids      = [aws_subnet.private["private-eks-1"].id, aws_subnet.private["private-eks-2"].id]
+
+  labels = {
+      "type" = "private"
+  }
+
+  instance_types = ["m5.large"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-group-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-group-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-group-AmazonEC2ContainerRegistryReadOnly
+  ]
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_eks_node_group" "public" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "public-node-group-${var.environment}"
+  node_role_arn   = aws_iam_role.node-group.arn
+  subnet_ids      = [aws_subnet.public["public-eks-1"].id, aws_subnet.public["public-eks-2"].id]
+
+  labels = {
+    "type" = "public"
+  }
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-group-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-group-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-group-AmazonEC2ContainerRegistryReadOnly
+  ]
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role" "node-group" {
+  name = "eks-node-group-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+      Statement = [{
+          Acction = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+              Service = "ec2.amazonaws.com"
+          }
+      }]
+      Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-group.name
+}
+
+resource "aws_iam_role_policy" "node-group-ClusterAutoscalerPolicy" {
+  name = "eks-cluster-auto-scaler"
+  role = aws_iam_role.node-group.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+            "autoscaling:DescribeAutoScalingGroups",
+            "autoscaling:DescribeAutoScalingInstances",
+            "autoscaling:DescribeLaunchConfigurations",
+            "autoscaling:DescribeTags",
+            "autoscaling:SetDesiredCapacity",
+            "autoscaling:TerminateInstanceInAutoScalingGroup"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
