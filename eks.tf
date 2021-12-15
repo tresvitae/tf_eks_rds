50f5fd1a6571423e06b498cb1cb417cf5b5ba25d
Patryk Futa <patryk.futa@gmail.com>
2021-12-15 14:03:00 +0100

    Add Role&Policy for cluster

--- eks.tf
+++ eks.tf
@@ -23,3 +23,32 @@ resource "aws_eks_cluster" "eks" {
     }
 }
 
+resource "aws_iam_role" "eks" {
+    name = "${var.eks_cluster_name}-${var.environment}"
+
+    assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks.name  
+}
+
